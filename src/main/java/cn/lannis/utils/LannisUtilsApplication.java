510a1473bd20dcaf60ae8e00413fa6b9a2602576
Lannis <623636245@qq.com>
2019-03-29 13:55:38 +0800

    初始化项目

---
+++ src/main/java/cn/lannis/utils/LannisUtilsApplication.java
@@ -0,0 +1,13 @@
+package cn.lannis.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LannisUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LannisUtilsApplication.class, args);
+	}
+
+}
